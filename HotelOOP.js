3d3957861fceab64b9e1cfcee033c0816856d80b
Martin Twum Mensah <martint.mensah@gmail.com>
2017-05-09 11:36:00 +0100

    Define properties of the HotelGuest class

--- HotelOOP.js
+++ HotelOOP.js
@@ -1,3 +1,10 @@
 /*A Github repo containing a real-world problem modeled using OOP while taking advantage of
  inheritance, encapsulation, polymorphism and the other OOP concepts.*/
- 
+function HotelGuest(fullName, age, gender, country, checkInDay, checkOutDay){
+  this.fullName = name;
+  this.age = age;
+  this.gender = gender;
+  this.country = country;
+  this.checkInDay = checkInDay;
+  this.checkOutDay = checkOutDay;
+}
